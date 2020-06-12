382089a8d0fc9f5e728e2292d7eee246527264ed
Lucas Ruy <lukas.r@hotmail.com>
2020-06-12 01:18:16 -0300

    refactor(test-redux): removed edit method and form from code

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import './styles/styles.css'
 
 import { configureStore } from './redux/store'
 
+import { EditModal } from 'components/EditModal'
 import { TestRedux } from 'components/TestRedux'
 
 const store = configureStore()
 
 const App = () => (
   <Provider store={store}>
+    <EditModal />
     <div>
       <h1>CodeLeap Network</h1>
       <p>This is initial structure to start mini-project using React & Redux.</p>
--- src/scripts/components/TestRedux/TestRedux.js
+++ src/scripts/components/TestRedux/TestRedux.js
@@ -2,11 +2,17 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import './test-redux.css'
+
 import * as postsActions from 'actions/modules/postsActions'
+import * as editModalActions from 'actions/ui/editModalActions'
 
 const TestRedux = () => {
   const dispatch = useDispatch()
   const posts = useSelector((state) => state.posts)
+
+  const openModal = (id) => {
+    dispatch(editModalActions.setModalOpen(id))
+  }
 
   const sendToCreatePost = (e) => {
     e.preventDefault()
@@ -16,19 +22,6 @@ const TestRedux = () => {
     dispatch(
       postsActions.createPost({
         username: formData.get('username'),
-        title: formData.get('title'),
-        content: formData.get('content'),
-      })
-    )
-  }
-
-  const sendToEditPost = (e) => {
-    e.preventDefault()
-
-    const formData = new FormData(e.target)
-
-    dispatch(
-      postsActions.editPost(formData.get('id'), {
         title: formData.get('title'),
         content: formData.get('content'),
       })
@@ -61,16 +54,6 @@ const TestRedux = () => {
         <button>Create a post</button>
       </form>
 
-      <form onSubmit={sendToEditPost}>
-        <label htmlFor="id">ID:</label>
-        <input type="text" id="id" name="id" />
-        <label htmlFor="title">Title:</label>
-        <input type="text" id="title" name="title" />
-        <label htmlFor="content">Content:</label>
-        <input type="text" id="content" name="content" />
-        <button>Edit post</button>
-      </form>
-
       <form onSubmit={sendToDeletePost}>
         <label htmlFor="id">ID:</label>
         <input type="text" id="id" name="id" />
@@ -79,7 +62,10 @@ const TestRedux = () => {
 
       <ul>
         {posts.get.payload.map((post) => (
-          <li key={post.id}>{post.title}</li>
+          <li key={post.id} onClick={() => openModal(post.id)}>
+            <h4>{post.title}</h4>
+            <p>{post.content}</p>
+          </li>
         ))}
       </ul>
     </div>
