eb5404a5e8d418a8656b4575df5d9c208e7ade82
Lucas Ruy <lukas.r@hotmail.com>
2020-06-12 18:43:17 -0300

    feat(login): created login page static

--- src/scripts/pages/Login/Login.js
+++ src/scripts/pages/Login/Login.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import './login.css'
+
+import { Input } from 'components/Input'
+import { Button } from 'components/Button'
+import { Card, CardHeader, CardContent, CardFooter } from 'components/Card'
 
 const Login = () => (
-  <div>
-    <h1>This is Login page of application.</h1>
+  <div className="login">
+    <Card>
+      <CardHeader title="Welcome to CodeLeap network!" />
+      <CardContent>
+        <Input type="text" label="Please enter your username" placeholder="John Doe" />
+      </CardContent>
+      <CardFooter align="right">
+        <Button>Enter</Button>
+      </CardFooter>
+    </Card>
   </div>
 )
 
