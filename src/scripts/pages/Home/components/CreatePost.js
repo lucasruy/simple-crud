4d5bc8e4b7d35226a5f5b14b260cacdbb9805f54
Lucas Ruy <lukas.r@hotmail.com>
2020-06-13 02:09:00 -0300

    feat(create-post): added method to send data via api and create a new post

--- src/scripts/pages/Home/components/CreatePost.js
+++ src/scripts/pages/Home/components/CreatePost.js
@@ -1,23 +1,45 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import * as postsActions from 'actions/modules/postsActions'
 
 import { Input } from 'components/Input'
 import { Button } from 'components/Button'
 import { Textarea } from 'components/Textarea'
 import { Card, CardHeader, CardContent, CardFooter } from 'components/Card'
 
-const CreatePost = () => (
-  <Card>
-    <CardHeader title="What's on your mind?" />
-    <CardContent>
-      <Input label="Title" name="title" placeholder="Hello world" withoutMargin />
-      <Textarea label="Content" name="content" placeholder="Content here" />
-    </CardContent>
-    <CardFooter align="right">
-      <Button type="button" withoutMargin>
-        Create
-      </Button>
-    </CardFooter>
-  </Card>
-)
+const CreatePost = () => {
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.user)
+
+  const createPost = (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.target)
+
+    dispatch(
+      postsActions.createPost({
+        username: user.payload.name,
+        title: formData.get('title'),
+        content: formData.get('content'),
+      })
+    )
+  }
+
+  return (
+    <form onSubmit={createPost}>
+      <Card>
+        <CardHeader title="What's on your mind?" />
+        <CardContent>
+          <Input label="Title" name="title" placeholder="Hello world" withoutMargin />
+          <Textarea label="Content" name="content" placeholder="Content here" />
+        </CardContent>
+        <CardFooter align="right">
+          <Button withoutMargin>Create</Button>
+        </CardFooter>
+      </Card>
+    </form>
+  )
+}
 
 export default CreatePost
