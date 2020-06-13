4ed7b757d5f6d7ac3df8e744c21a800e615f1451
Lucas Ruy <lukas.r@hotmail.com>
2020-06-13 00:36:49 -0300

    feat(store): added redux devtools

--- src/scripts/redux/store.js
+++ src/scripts/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 
 import rootSaga from 'sagas/'
@@ -6,7 +6,9 @@ import rootReducer from './rootReducer'
 
 export const configureStore = () => {
   const sagaMiddleware = createSagaMiddleware()
-  const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+  const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)))
 
   sagaMiddleware.run(rootSaga)
 
