910c9b1fc07861930cb75a0e61e99dce2d77810e
Lucas Ruy <lukas.r@hotmail.com>
2020-06-13 12:26:51 -0300

    feat(post-list): created post list component

--- src/scripts/pages/Home/Home.js
+++ src/scripts/pages/Home/Home.js
@@ -5,7 +5,7 @@ import { EditModal } from 'components/EditModal'
 import { DeleteAlert } from 'components/DeleteAlert'
 import { Card, CardHeader, CardContent } from 'components/Card'
 
-import { Post } from './components/Post'
+import PostList from './components/PostList'
 import CreatePost from './components/CreatePost'
 
 const Home = () => (
@@ -16,17 +16,7 @@ const Home = () => (
       <CardHeader title="CodeLeap Network" background="black" />
       <CardContent>
         <CreatePost />
-        <ul>
-          <li>
-            <Post
-              id={52}
-              title="My first post at CodeLeap NetWork"
-              content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-              author="Lucas"
-              date="1 minute ago"
-            />
-          </li>
-        </ul>
+        <PostList />
       </CardContent>
     </Card>
   </div>
---
+++ src/scripts/pages/Home/components/PostList.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import * as postsActions from 'actions/modules/postsActions'
+
+import { Post } from './Post'
+
+const PostList = () => {
+  const dispatch = useDispatch()
+  const posts = useSelector((state) => state.posts)
+
+  useEffect(() => {
+    dispatch(postsActions.getAllPosts())
+  }, [])
+
+  return (
+    <ul>
+      {posts.get.payload.map((post) => (
+        <li key={post.id}>
+          <Post id={post.id} title={post.title} content={post.content} author={post.username} date={post.created_datetime} />
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default PostList
