89985bb6bf39ab86b838849540a335bfb53e5b14
Lucas Ruy <lukas.r@hotmail.com>
2020-06-13 18:51:03 -0300

    feat(no-match): created style of search not found page

--- src/scripts/pages/NoMatch/NoMatch.js
+++ src/scripts/pages/NoMatch/NoMatch.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './no-match.css'
 
+import { Button } from 'components/Button'
 import { FadeTransition } from 'components/FadeTransition'
+import { Card, CardHeader, CardContent } from 'components/Card'
 
 const NoMatch = () => (
   <FadeTransition>
-    <div>
-      <h1>404</h1>
-      <p>No match found for your search.</p>
+    <div className="no-match">
+      <Card>
+        <CardHeader title="404" background="black" />
+        <CardContent>
+          <h3>Page not found</h3>
+          <p>It looks like you&apos;re lost</p>
+          <Button type="button">
+            <Link to="/home">Take me out of here</Link>
+          </Button>
+        </CardContent>
+      </Card>
     </div>
   </FadeTransition>
 )
