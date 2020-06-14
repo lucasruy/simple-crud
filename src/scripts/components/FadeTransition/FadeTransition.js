b7ae36c642f456cf6d30c9386b8ddac5ba0bc860
Lucas Ruy <lukas.r@hotmail.com>
2020-06-14 11:47:02 -0300

    chore(fade-transition): added duration prop to fade transition component

--- src/scripts/components/FadeTransition/FadeTransition.js
+++ src/scripts/components/FadeTransition/FadeTransition.js
@@ -2,22 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { motion } from 'framer-motion'
 
-const FadeTransition = ({ children }) => {
+const FadeTransition = ({ children, duration }) => {
   const fadeAnim = {
     animate: { opacity: 1 },
     initial: { opacity: 0 },
     exit: { opacity: 0 },
     transition: {
       ease: 'easeInOut',
-      duration: 0.3,
+      duration,
     },
   }
 
   return <motion.div {...fadeAnim}>{children}</motion.div>
 }
 
+FadeTransition.defaultProps = {
+  duration: 0.3,
+}
+
 FadeTransition.propTypes = {
   children: PropTypes.node.isRequired,
+  duration: PropTypes.number,
 }
 
 export default FadeTransition
